2ddf39d8049e73c6c563beb445fe652eb6df4341
David Velasquez <dvelasquez2@directv.com>
2015-09-09 11:54:42 -0700

    Simplified controller (moved function to class.js)

--- controller/controller.js
+++ controller/controller.js
@@ -1,14 +1,5 @@
 angular.module('cablingDiagram').controller('cablingDiagramController', ['$scope', function($scope) {
     $scope.portfolioArray = data;
     $scope.connectionArray = getConnections($scope.portfolioArray);
-    $scope.checkChange = function(connections) {
-        var checked = [];
-        for (var t in connections) {
-            if (connections[t].on === true) {
-                pushUnique(connections[t].name, checked);
-            }
-        }
-        checked.sort();
-        return checked;
-    };
+    $scope.checkChange = getChecked($scope.connectionArray);
 }]);
