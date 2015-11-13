4d36ab046b7f79c661c32ee3502288c097652735
David Velasquez <DVelasquez2@directv.com>
2015-11-13 12:11:11 -0800

    Correcting S-Video compatibility with HR44, HR54

--- data/data.js
+++ data/data.js
@@ -246,7 +246,7 @@ receiverData = [{
         2: true,
         3: false,
         4: true,
-        5: true,
+        5: false,
         6: false,
         7: true,
         8: true
@@ -258,7 +258,7 @@ receiverData = [{
         2: true,
         3: false,
         4: true,
-        5: true,
+        5: false,
         6: false,
         7: true,
         8: true
